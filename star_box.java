e776fbaab1bd191d247cd6efa6989679af6bdd34
Rajesh Ananta Patil <139704615+Tejraj0319@users.noreply.github.com>
2023-07-29 22:16:07 +0530

    Add files via upload

---
+++ star_box.java
@@ -0,0 +1,23 @@
+package Star;
+
+import java.util.Scanner;
+
+public class star_box {
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the range: ");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
