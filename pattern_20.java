affa3fa746fa472f67e7157fae3800a9d7673ee9
Rajesh Ananta Patil <139704615+Tejraj0319@users.noreply.github.com>
2023-08-21 11:00:18 +0530

    Update and rename Day_20.java to pattern_20.java

---
+++ pattern_20.java
@@ -0,0 +1,27 @@
+
+public class pattern_20 {
+    public static void main(String[] args) 
+    {
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            for( int  j=i;j<=n;j++)
+            {
+                System.out.print("  ");
+            }
+            for(int k=1;k<=i;k++)
+            {
+                if(i==n||k==1||k==i)
+                {
+                   System.out.print("* "); 
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
